d2864e0bc35b5e3986e2979bc14f580f692a108c
Federico Medina <pardo.medina@gmail.com>
2016-05-20 22:40:58 -0400

    Remove a useless closure
    Prevents code optimization. Not that it matters much, though.

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 module.exports = function(Promise){
     if (typeof Promise === 'function' && !Promise.prototype.whoops) {
-        Promise.prototype.whoops = function() {
-            Promise.prototype['catch'].apply(this, arguments);
-        };
+        Promise.prototype.whoops = Promise.prototype['catch'];
     }
     return Promise;
 }
